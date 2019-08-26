dc1fd2d19c97b92b3049382f4fa0a0652b688573
berkansivri <berkansivri@gmail.com>
2019-08-26 10:40:59 +0300

    styling

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
     contentLabel="Selected Option"
     onRequestClose={props.clearSelectedOption}
     ariaHideApp={false}
+    closeTimeoutMS={300}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.clearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.clearSelectedOption}>Okay</button>
   </Modal>
 )
 
