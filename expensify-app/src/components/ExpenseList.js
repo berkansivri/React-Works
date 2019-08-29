bf9659ade7e547313748bdf94412beba6a37b18c
berkansivri <berkansivri@gmail.com>
2019-08-29 17:09:56 +0300

    react component snapshot testing. added react-test-renderer & enzyme. test configurations

--- expensify-app/src/components/ExpenseList.js
+++ expensify-app/src/components/ExpenseList.js
@@ -3,10 +3,15 @@ import { connect } from 'react-redux'
 import ExpenseListItem from './ExpenseListItem'
 import selectExpenses from '../selectors/expenses'
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
   <div>
-    <h1>Expense List</h1>
-    {props.expenses.map(ex => <ExpenseListItem key={ex.id} {...ex} />)}
+    {
+      props.expenses.length === 0 ? (
+        <p>No expenses</p>
+      ) : (
+        props.expenses.map(ex => <ExpenseListItem key={ex.id} {...ex} />)
+      )
+    }
   </div>
 )
 
--- expensify-app/src/tests/reducers/expenses.test.js
+++ expensify-app/src/tests/reducers/expenses.test.js
@@ -8,27 +8,34 @@ test("should set default state", () => {
 })
 
 test("should remove expense by id", () => {
-  const action = { id: '2', type: "REMOVE_EXPENSE" }
+  const action = { id: expenses[1].id, type: "REMOVE_EXPENSE" }
   const state = expensesReducer(expenses, action)
   expect(state).toEqual([ expenses[0], expenses[2] ])
 })
 
-test("should remove expense by id", () => {
+test("should not remove expense without id", () => {
   const action = { id: '-1', type: "REMOVE_EXPENSE" }
   const state = expensesReducer(expenses, action)
-  expect(state).toEqual([ expenses[0], expenses[2] ])
+  expect(state).toEqual(expenses)
 })
 
 test("should add expense", () => {
-  const expense = { id: '4', description: 'Something', note: '', amount: 195, createdAt: moment().valueOf() }
+  const expense = { id: '101', description: 'Something', note: '', amount: 195, createdAt: moment().valueOf() }
   const action = { expense, type: "ADD_EXPENSE" }
   const state = expensesReducer(expenses, action)
   expect(state).toEqual([ ...expenses, expense ])
 })
 
-test("should edit expense", () => {
+test("should edit an expense", () => {
   const updateExpense = { description: 'Something', note: '', amount: 500, createdAt: moment().valueOf() }
-  const action = { id:"3", updateExpense, type: "EDIT_EXPENSE" }
+  const action = { id: expenses[2].id, updateExpense, type: "EDIT_EXPENSE" }
   const state = expensesReducer(expenses, action)
   expect(state).toEqual([ expenses[0], expenses[1], { ...expenses[2], ...updateExpense } ])
+})
+
+test("should not edit expense without id", () => {
+  const updateExpense = { description: 'Something', note: '', amount: 500, createdAt: moment().valueOf() }
+  const action = { id: "-1", updateExpense, type: "EDIT_EXPENSE" }
+  const state = expensesReducer(expenses, action)
+  expect(state).toEqual(expenses)
 })
