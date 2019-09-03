dbbd741e9fba83211c86eca86f2ebb623a55fd10
berkansivri <berkansivri@gmail.com>
2019-09-03 15:19:51 +0300

    setup test database environment and test to async redux and db

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACmxMMnPc-hZ1xQfQsmjV66R7lu99uiq8",
-  authDomain: "expensify-1ae3c.firebaseapp.com",
-  databaseURL: "https://expensify-1ae3c.firebaseio.com",
-  projectId: "expensify-1ae3c",
-  storageBucket: "",
-  messagingSenderId: "5794495904",
-  appId: "1:5794495904:web:5d0f43c4eb6e8402"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)
