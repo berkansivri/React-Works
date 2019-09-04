a05aeb1e62cbbda90a3a8afb2f3272420f7a29cd
berkansivri <berkansivri@gmail.com>
2019-09-04 17:05:17 +0300

    added private route

--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -7,6 +7,7 @@ export const Header = ({ logout }) => (
   <header>
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>Home </NavLink>
+    <NavLink to="/dashboard" activeClassName="is-active">Dashboard </NavLink>
     <NavLink to="/create" activeClassName="is-active">Create </NavLink>
     <NavLink to="/help" activeClassName="is-active">Help </NavLink>
     <button onClick={logout}>Logout</button>
--- expensify-app/src/routers/AppRouter.js
+++ expensify-app/src/routers/AppRouter.js
@@ -1,25 +1,24 @@
 import React from 'react' 
 import { Router, Switch, Route } from 'react-router-dom'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
-import Header from '../components/Header'
 import NotFoundPage from '../components/NotFoundPage'
 import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
 import HelpPage from '../components/HelpPage'
 import LoginPage  from '../components/LoginPage';
 import { createBrowserHistory } from 'history'
+import PrivateRoute from './PrivateRoute'
 
 export const history = createBrowserHistory()
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact={true} />
-        <Route path="/dashboard" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
+        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
